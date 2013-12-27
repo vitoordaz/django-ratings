362b0d5c2c7a20ff370cbc10d80979dfe5e56f42
Victor Rodionov <vito.ordaz@gmail.com>
2013-12-26 21:25:34 -0800

    Added for django 1.5 custom auth user model
    This patch adds support for Django 1.5 custom user mode support, that
    gives chance user redefine default authorization user model.

--- djangoratings/models.py
+++ djangoratings/models.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 from django.db import models
+from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
-from django.contrib.auth.models import User
 
 try:
     from django.utils.timezone import now
@@ -12,12 +12,17 @@ except ImportError:
 
 from managers import VoteManager, SimilarUserManager
 
+
+# NOTE: By default this value is auth.User
+AUTH_USER_MODEL = settings.AUTH_USER_MODEL
+
+
 class Vote(models.Model):
     content_type    = models.ForeignKey(ContentType, related_name="votes")
     object_id       = models.PositiveIntegerField()
     key             = models.CharField(max_length=32)
     score           = models.IntegerField()
-    user            = models.ForeignKey(User, blank=True, null=True, related_name="votes")
+    user            = models.ForeignKey(AUTH_USER_MODEL, blank=True, null=True, related_name="votes")
     ip_address      = models.IPAddressField()
     cookie          = models.CharField(max_length=32, blank=True, null=True)
     date_added      = models.DateTimeField(default=now, editable=False)
@@ -55,7 +60,7 @@ class Score(models.Model):
     key             = models.CharField(max_length=32)
     score           = models.IntegerField()
     votes           = models.PositiveIntegerField()
-    
+
     content_object  = generic.GenericForeignKey()
 
     class Meta:
@@ -65,14 +70,14 @@ class Score(models.Model):
         return u"%s scored %s with %s votes" % (self.content_object, self.score, self.votes)
 
 class SimilarUser(models.Model):
-    from_user       = models.ForeignKey(User, related_name="similar_users")
-    to_user         = models.ForeignKey(User, related_name="similar_users_from")
+    from_user       = models.ForeignKey(AUTH_USER_MODEL, related_name="similar_users")
+    to_user         = models.ForeignKey(AUTH_USER_MODEL, related_name="similar_users_from")
     agrees          = models.PositiveIntegerField(default=0)
     disagrees       = models.PositiveIntegerField(default=0)
     exclude         = models.BooleanField(default=False)
-    
+
     objects         = SimilarUserManager()
-    
+
     class Meta:
         unique_together = (('from_user', 'to_user'),)
 
@@ -80,14 +85,14 @@ class SimilarUser(models.Model):
         print u"%s %s similar to %s" % (self.from_user, self.exclude and 'is not' or 'is', self.to_user)
 
 class IgnoredObject(models.Model):
-    user            = models.ForeignKey(User)
+    user            = models.ForeignKey(AUTH_USER_MODEL)
     content_type    = models.ForeignKey(ContentType)
     object_id       = models.PositiveIntegerField()
-    
+
     content_object  = generic.GenericForeignKey()
-    
+
     class Meta:
         unique_together = (('content_type', 'object_id'),)
-    
+
     def __unicode__(self):
         return self.content_object
